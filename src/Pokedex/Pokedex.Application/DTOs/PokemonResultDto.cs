8d12f39489931de7946ec7f78e4ac486339e0656
Pedro <pedrolmoreira99@gmail.com>
2026-06-10 04:31:27 -0300

    First Config in Application

---
+++ src/Pokedex/Pokedex.Application/DTOs/PokemonResultDto.cs
@@ -0,0 +1,20 @@
+namespace Pokedex.Application.DTOs;
+
+
+[GraphQLName("PokemonResult")]
+public sealed class PokemonResultDto
+{
+    public int Number { get; set; }
+    public string? Name{ get; set; }
+    public string? Type1 { get; set; }
+    public string? Type2 { get; set; }
+    public int? Total { get; set; }
+    public int? Hp { get; set; }
+    public int? Attack { get; set; }
+    public int? Defense { get; set; }
+    public int? SpAttack { get; set; }
+    public int? SpDefense { get; set; }
+    public int? Speed { get; set; }
+    public int? Generation { get; set; }
+    public bool? Legendary { get; set; }
+}
