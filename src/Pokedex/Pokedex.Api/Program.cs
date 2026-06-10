b6bca0d304772f8ef07f61f7530cedaa427c864c
Pedro <pedrolmoreira99@gmail.com>
2026-06-10 04:31:49 -0300

    refact: Api to SDL

--- src/Pokedex/Pokedex.Api/Program.cs
+++ src/Pokedex/Pokedex.Api/Program.cs
@@ -1,12 +1,29 @@
+using Pokedex.Application.DTOs;
+using Pokedex.Application.Resolvers;
+using Pokedex.Infrastructure;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
 
+builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddProblemDetails();
+builder.Services.AddHttpContextAccessor();
 builder.Services
-    .AddGraphQLServer();
+    .AddGraphQLServer()
+    .AddDocumentFromFile("../Pokedex.Infrastructure/Schema/pokedex.graphql")
+    .AddResolver<QueryPokemonGenericResolver>("Query")
+    .BindRuntimeType<PokemonDataDto>("PokemonData")
+    .BindRuntimeType<PokemonResultDto>("PokemonResult");
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<PokedexDbContext>();
+    db.Database.Migrate();
+}
 
 app.MapGraphQL("/pokedex");
 app.MapDefaultEndpoints();
